a7c5874bc860de6bb6c6966effc0cebc4636a7cd
zainulla.devops <zainulla.devops@gmail.com>
2018-05-21 15:19:53 +0530

    .

--- build/resources/main/jetty/uninstall-app.sh
+++
@@ -1,9 +0,0 @@
-echo reinstalling archive ${deployed.deployable.file} in ${deployed.container.home}
-echo Stop the service...
-echo ${deployed.container.serviceStop}
-echo Removing old artifacts
-rm -rf ${deployed.container.targetDirectory}/${deployed.container.artifactsName}
-echo Deploying new artifact
-cp ${deployed.deployable.file} ${deployed.container.targetDirectory}/${deployed.container.artifactsName}
-echo Start the service...
-${deployed.container.serviceStart}
