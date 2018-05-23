847d7df5fadb7eeeaa63d8d565c1116e2aa3be39
zainulla.devops <zainulla.devops@gmail.com>
2018-05-23 13:25:33 +0530

    updated uninstallation script

--- src/main/resources/jetty/uninstall-app.sh
+++ src/main/resources/jetty/uninstall-app.sh
@@ -1,9 +1,5 @@
-echo reinstalling archive ${deployed.deployable.file} in ${deployed.container.home}
+echo removing deployment
 echo Stop the service...
 echo ${deployed.container.serviceStop}
-echo Removing old artifacts
+echo Removing artifacts
 rm -rf ${deployed.container.targetDirectory}/${deployed.container.artifactsName}
-echo Deploying new artifact
-cp ${deployed.deployable.file} ${deployed.container.targetDirectory}/${deployed.container.artifactsName}
-echo Start the service...
-${deployed.container.serviceStart}
